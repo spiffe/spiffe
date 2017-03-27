1ac61165be1c5ca1f8e344824d4daf225007b870
emiliano <emiliano@medlista.com>
2017-03-27 16:59:27 -0700

    First Pass
    Adding SPIFFE x509 Extensions. Factor out configurations to ini file

--- python/spiffe/spiffe.py
+++ python/spiffe/spiffe.py
@@ -1,6 +1,22 @@
-from datetime import datetime, timedelta
-import sys
+"""Spiffe Leaf PKI.
 
+Usage:
+  spiffe.py --id=<spiffe_id> --path=<cert_path> --pass=<pass> --secret=<secret> --config=<config>
+  spiffe.py (-h | --help)
+  spiffe.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version               Show version.
+  --id=<spiffe_id>        SPIFFE ID to user in certificate.
+  --path=<cert_path>      Path to intermediate certificates
+  --pass=<pass>           Passpharse for certificate
+  --secret=<secret>       Secret for intermediate certificates
+  --config=<config file>  Config File that loads all configurations for SPIFFE
+                          certificate generation
+
+
+"""
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -8,10 +24,19 @@ from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives import hashes
 
-def generate_spiffe(identity,
+from datetime import datetime, timedelta
+from docopt import docopt
+
+import sys
+from config import SpiffeConfig
+
+def generate_spiffe(spiffe_config,
+                    identity,
                     passphrase,
                     intermediate_path,
                     intermediate_secret):
+    """
+    """
 
     key = rsa.generate_private_key(
         public_exponent=65537,
@@ -20,11 +45,11 @@ def generate_spiffe(identity,
     )
 
     csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
-        x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
-        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"CA"),
-        x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
-        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"Twilio"),
-        x509.NameAttribute(NameOID.COMMON_NAME, u"PA Spiffe Cert"),
+        x509.NameAttribute(NameOID.COUNTRY_NAME, spiffe_config.country),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, spiffe_config.state_or_province),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, spiffe_config.locality),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, spiffe_config.organization),
+        x509.NameAttribute(NameOID.COMMON_NAME, spiffe_config.common_name),
     ])).add_extension(
         x509.SubjectAlternativeName([
             # Describe what sites we want this certificate for.
@@ -34,8 +59,8 @@ def generate_spiffe(identity,
         # Sign the CSR with our private key.
     ).sign(key, hashes.SHA256(), default_backend())
 
-    # sign the csr with the intermediate cert
 
+    # sign the csr with the intermediate cert
     intermediate_key = None
     with open("{}/private/intermediate.key.pem".format(intermediate_path), 'rb') as f:
         intermediate_key = default_backend().load_pem_private_key(f.read(), intermediate_secret)
@@ -44,24 +69,47 @@ def generate_spiffe(identity,
     with open("{}/certs/intermediate.cert.pem".format(intermediate_path), 'rb') as f:
         intermediate_cert = x509.load_pem_x509_certificate(f.read(), default_backend())
 
-    cert = x509.CertificateBuilder().subject_name(
-        csr.subject
-    ).issuer_name(
-        intermediate_cert.subject
-    ).public_key(
-        key.public_key()
-    ).serial_number(
-        x509.random_serial_number()
-    ).not_valid_before(
-        datetime.utcnow()
-    ).not_valid_after(
+    cert = x509.CertificateBuilder().subject_name(csr.subject)
+
+    cert = cert.issuer_name(intermediate_cert.subject)
+    cert = cert.public_key(key.public_key())
+
+    cert = cert.serial_number(x509.random_serial_number())
+
+    cert = cert.not_valid_before(datetime.utcnow())
+    cert = cert.not_valid_after(
         # Our certificate will be valid for 10 days
-        datetime.utcnow() + timedelta(days=10)
-    ).add_extension(
+        datetime.utcnow() + timedelta(days=10))
+
+
+    # x509 Extensions
+    cert = cert.add_extension(
         x509.SubjectAlternativeName([x509.UniformResourceIdentifier(identity)]),
-        critical=False,
-        # Sign our certificate with our private key
-    ).sign(intermediate_key, hashes.SHA256(), default_backend())
+        critical=False,)
+
+    cert = cert.add_extension(x509.BasicConstraints(ca=False,path_length=None),
+                              critical=True)
+
+    name_constraints = []
+    for constraint in spiffe_config.name_contraints:
+
+        name_constraints.append(x509.UniformResourceIdentifier(constraint))
+
+    cert = cert.add_extension(x509.NameConstraints(permitted_subtrees=name_constraints,
+                                                   excluded_subtrees=None),
+                              critical=False)
+
+    cert.add_extension(x509.ExtendedKeyUsage([x509.oid.ExtendedKeyUsageOID.CLIENT_AUTH,
+                                                    x509.oid.ExtendedKeyUsageOID.SERVER_AUTH]),
+                              critical=False)
+
+    cert.add_extension(x509.SubjectKeyIdentifier(),
+                       critical=False)
+
+    cert.add_extension(x509.AuthorityKeyIdentifier(),
+                       critical=False)
+    # Sign our certificate with our private key
+    cert = cert.sign(intermediate_key, hashes.SHA256(), default_backend())
 
 
     private_out = key.private_bytes(
@@ -73,7 +121,11 @@ def generate_spiffe(identity,
 
     return private_out, certificate
 
-def generate_spiffe_to_file(private_key, certificate, file_prefix):
+def generate_spiffe_to_file(private_key,
+                            certificate,
+                            file_prefix):
+    """
+    """
     with open("{}.key.pem".format(file_prefix), 'wb') as f:
         f.write(private_key)
 
@@ -82,11 +134,21 @@ def generate_spiffe_to_file(private_key, certificate, file_prefix):
 
 
 if __name__ == '__main__':
-    path_to_ca = sys.argv[1]
-    p_key, certificate = generate_spiffe("urn:spiffe:service:acme.com:acme-dev:foo-service",
-                                         b"secret",
-                                         path_to_ca,
-                                         b'secret')
+
+    args = docopt(__doc__, version='Spiffe PKI 1.0')
+
+    print(args)
+
+    spiffe_local_config = SpiffeConfig(args["--config"])
+    spiffe_local_config.load()
+
+    # "urn:spiffe:service:acme.com:acme-dev:foo-service"
+    p_key, certificate = generate_spiffe(spiffe_config=spiffe_local_config,
+                                         identity=args["--id"],
+                                         passphrase=bytes(args["--pass"], 'utf-8'),
+                                         intermediate_path=args["--path"],
+                                         intermediate_secret=bytes(args["--secret"], 'utf-8'))
+
     generate_spiffe_to_file(p_key, certificate, 'spiffe')
 
 
